43e2ed26f856d055df3f7369f3b05685817a0e94
Strokow <aleksestroko@yandex.com>
2024-04-07 13:52:16 +0200

    update 0704 header is done

--- src/App.js
+++ src/App.js
@@ -4,37 +4,30 @@ function App() {
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img height={80} weight={80} src="img/lookbooklogo.png" alt="Lookbook Logo" />
-
-        
-        
         </div>
         <ul className="d-flex justify-center">
-        <div className="d-flex justify-between">
-          <h3 className="text-uppercase">Log In</h3>
-          <h3 className="text-uppercase">Sign In</h3>
-          </div>
-         
-          <div className="d-flex mr-30 justify-between justify-between">
-          <li>
-          <img height={30} weight={30} src="img/korblogo2.png" alt="Cart" />
-          <span>
-            19€
-          </span>
-          </li>
-          <li>
-          <img height={30} weight={30} src="img/usericon.png" alt="Profile" />
-          </li>
+          <div className="d-flex justify-between">
+          <h3 className="text-uppercase" style={{ marginRight: '15px' }}>Log In</h3>
+          <h3 className="text-uppercase" style={{ marginRight: '15px' }}>Sign In</h3>
           </div>
 
+          <div className="d-flex mr-30 align-center justify-between justify-between">
+            <li className="d-flex align-center">
+              <img height={30} weight={30} src="img/korblogo2.png" alt="Cart" />
+              <span style={{ marginRight: '15px' }}>
+                19€
+              </span>
+            </li>
+            <li>
+              <img height={30} weight={30} src="img/usericon.png" alt="Profile" />
+            </li>
+          </div>
         </ul>
       </header>
       <div className="content p-40">
         <h1>All books</h1>
         <p>Here will be some content
-          
-
         </p>
-        
       </div>
     </div>
   );
