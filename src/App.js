fcfb13a225b3248abbfd0359e9329c3b3969126c
Strokow <aleksestroko@yandex.com>
2024-04-08 18:02:40 +0200

    footer final FINAL

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -34,19 +36,19 @@ function App() {
         <p>lookbook@examplemail.com</p>
         <p className="mb-25">Tel.01722223340</p>
         
-        <a className="facebook" href="https://www.facebook.com/">
+        <a className="facebook" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
         <img className="mr-10" height={30} weight={30} src="img/fb.png" alt="Facebook" />
         </a>
         
         
-        <a className="whatsapp" href="https://web.whatsapp.com/">
+        <a className="whatsapp" href="https://web.whatsapp.com/" target="_blank" rel="noopener noreferrer">
         <img className="mr-10" height={30} weight={30} src="img/wp.png" alt="WhatsApp" />
-    </a>
+       </a>
         
     
-    <a className="instagram" href="https://www.instagram.com/">
+       <a className="instagram" href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
         <img className="mr-10" height={30} weight={30} src="img/ig.png" alt="Instagram" />
-    </a>
+       </a>
     
         
       </footer>
