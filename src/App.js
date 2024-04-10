4595d9ca8ae1b3758ba2e0ce50bf00f002f86828
Strokow <aleksestroko@yandex.com>
2024-04-10 12:05:11 +0200

    cards and backend

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import React from 'react';
-import Card from './components/Card';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React, { useState, useEffect } from "react";
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
+  const [books, setBooks] = useState([]);
+  async function loadBooks() {
+    const res = await fetch("/api/book/forusername/active");
+    const obj = await res.json();
+    console.log(obj);
+    setBooks(obj);
+  }
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
   return (
     <div className="wrapper clear">
       <Header />
       <div className="search-container">
-        <input className="input-search" type="text"/>
-        <img className="search-icon" height={20} width={20} src="/img/search.png" alt="Search" />
+        <input className="input-search" type="text" />
+        <img
+          className="search-icon"
+          height={20}
+          width={20}
+          src="/img/search.png"
+          alt="Search"
+        />
       </div>
       <div className="content p-40">
         <h1 className="mb-40">Our book range</h1>
-        <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />      
+        <div className="d-flex flex-wrap justify-between">
+          {books.map((book) => (
+            <div style={{ marginBottom: "20px" }}>
+              <Card book={book} />
+            </div>
+          ))}
         </div>
       </div>
-      <div>
       <Footer />
-      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,20 @@
-function Card() {
+import React from 'react';
+function Card({book}) {
     return (
-        <div className="card">
-            <img height={150} width={150} src="/img/crime.jpg" alt="Crime and Punishment"/>
-            <h5>«Crime and punishment» Fyodor Dostoevsky</h5>
+        <div className="card" key={'book'+ book.id}>
+            <img height={160} width={160} src={`/api/img/get/${book.pathimg}`} alt={book.name}/>
+            <h5>{book.name}, {book.author}</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
-                <b>19€</b>
+                <b>{book.price}€</b>
               </div>
               <button className="button">
                 <img height={20} width={20} src="/img/greyplus.png" alt="Add"/>
               </button>
             </div>
-            
-          </div>
+        </div>
     )
 }
+
 
 export default Card
