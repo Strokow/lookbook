280e074ecfb30debe5f7b59dc049e7a807e3dc32
fogroy2000 <livetrade2000@gmail.com>
2024-04-21 16:25:27 +0200

    add button in ordered

--- src/pages/Ordered.jsx
+++ src/pages/Ordered.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import {useNavigate } from 'react-router-dom';
 
 function Ordered() {
+  const navigate = useNavigate();
   return (
     <div className="wrapper clear">
+      <button className="BackToButton ml-40" onClick={() => navigate('/')}>Back to startpage</button>
       <div className="content p-40">
         <h1 className="mb-40" style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>Thank you for your purchase!</h1>
         <div className="d-flex flex-wrap justify-between">
